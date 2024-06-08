be0ae04db13a41a7e5f7079755a0a29a442aff03
helladyo <heladiofog@gmail.com>
2024-06-08 09:11:21 -0600

    Agregar el modelo de User para la seguridad.

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    encryptPassword: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
